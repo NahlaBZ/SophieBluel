f67c7b3aa2475ce911dc77bb308fb33c0057b13a
NahlaBZ <benzidnahla@gmail.com>
2023-03-09 23:31:47 +0100

    login rectifié

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,59 +1,80 @@
-// CONST VERS L'API //
-const apiWorks = "http://localhost:5678/api/";
+const loginUrl = "http://localhost:5678/api/users/login";
+const inputEmail = document.getElementById("email");
+const inputPassword = document.getElementById("password");
+const form = document.getElementById("loginForm");
+const loginError = document.querySelector(".loginError");
+var data;
 
-// Variable email,password,form //
-let emailInput = document.querySelector("#email");
-let passwordInput = document.querySelector("#password");
-let form = document.querySelector("form");
+const logUser = {
+    email: "",
+    password: "",
+};
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    // const pour avoir la value de password et de l'email //
-    const userlogin = {
-        email: emailInput.value,
-        password: passwordInput.value,
-    };
-    fetch(apiWorks + "users/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json;charset=utf-8",
-        },
-        body: JSON.stringify(userlogin),
-    })
-        // Message d'erreurs //
-        .then((response) => {
-            if (!response.ok) {
-                let ExistingErrorContainer = document.querySelector(".error_container");
-                if (ExistingErrorContainer) {
-                    form.removeChild(ExistingErrorContainer);
-                }
-
-                // Affichage Erreur //
-                const errorContainer = document.createElement("div");
-                errorContainer.classList.add("error_container");
-                const connexionInput = form.querySelector('input[type="submit"]');
-                form.insertBefore(errorContainer, connexionInput);
-
-                if (response.status === 404) {
-                    errorContainer.innerText =
-                        "Erreur dans l’identifiant ou le mot de passe";
-                }
-                if (response.status === 401) {
-                    errorContainer.innerText =
-                        "Erreur dans l’identifiant ou le mot de passe";
-                }
-            } else {
-                return response.json();
-            }
-        })
-
-        //Stockage  userId,token ET redirection //
-        .then((data) => {
-            localStorage.setItem("id", data.userId);
-            localStorage.setItem("token", data.token);
-            document.location.href = "index.html";
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+//Quand on clique sur le bouton "Se connecter"
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    loginUser();
 });
+
+// Action suite à la saisie de l'email
+inputEmail.addEventListener("input", (e) => {
+    //inputEmail.reportValidity();
+    inputPassword.style.color = "#1d6154";
+    inputEmail.style.color = "#1d6154";
+    logUser.email = e.target.value;
+});
+
+// Action suite à la saisie du mot de passe
+inputPassword.addEventListener("input", (e) => {
+    //inputPassword.reportValidity();
+    inputPassword.style.color = "#1d6154";
+    inputEmail.style.color = "#1d6154";
+    logUser.password = e.target.value;
+});
+
+//Au chargement de la page
+document.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault();
+    //logUser.email = inputEmail.value;
+    //logUser.password = inputPassword.value;
+    //console.log(logUser);
+});
+
+// Appel de la fonction login
+async function loginUser() {
+    try {
+        data = await fetch(loginUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(logUser),
+        }).then((response) => response.json());
+
+        //console.log(data);
+
+        if (data.message || data.error) {
+            loginError.textContent = "Erreur dans l’identifiant ou le mot de passe";
+            inputEmail.style.color = "red";
+            inputPassword.style.color = "red";
+
+        } else { //Si le login / mot de passe sont corrects
+
+            //inputPassword.style.color = "#1d6154";
+            //inputEmail.style.color = "#1d6154";
+            //loginError.textContent = "";
+
+            //console.log("LogAdmin OK");
+            //console.log(logUser);
+
+            // stockage du token dans le stockage local
+            localStorage.setItem("token", data.token);
+
+            //Redirection vers index.html
+            window.location.href = "./index.html";
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
