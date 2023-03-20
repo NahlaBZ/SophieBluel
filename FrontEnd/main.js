023687d9c1063e92a794c9a4028a798b7ac6d369
NahlaBZ <benzidnahla@gmail.com>
2023-03-20 20:06:17 +0100

    Modal(affichage + ajout travaux)

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,4 +1,5 @@
 const backEndApiURL = "http://localhost:5678/api/";
+let works;
 const token = localStorage.getItem("token");
 let selectedCategory = "Tous";
 const filterButtons = document.querySelector(".filter");
@@ -140,8 +141,13 @@ async function fetchApiCategories() {
 window.addEventListener("load", () => {
     //On appelle la fonction fetchWorks au chargement de la page HTML
     fetchWorks();
+    fetchApiCategories();
     checkToken();
 });
+
+
+
+
 
 
 
@@ -162,25 +168,30 @@ function removeToken() {
     // Supprime le token du localStorage
     localStorage.removeItem("token");
 
+
 }
 
 //événement fermeture onglet ou redirection vers un autre site
 window.addEventListener("unload", removeToken);
 
+
+
+
 // Affichage du mode admin
 function adminmode() {
     adminHTML();
 
+    const modalJs = document.getElementById("titleProjectEdition");
 
-
-
-    //SUPRESSION DES TRAVAUX DE L'API
-    const deleteWorksApi = document.querySelector("body > div > button");
-    //Confirmation DELETE CARTES dans L'API
-    deleteWorksApi.addEventListener("click", (e) => {
+    modalJs.addEventListener("click", (e) => {
         e.preventDefault();
-        functionDeleteWorksApi();
+        modalHTML();
+        displayModal();
+        openModal();
+        AddpicModal();
     });
+
+
 }
 
 const adminHTML = () => {
@@ -194,8 +205,8 @@ const adminHTML = () => {
         .insertAdjacentElement("afterbegin", blackEditor);
 
     const spanblackEditor = document.createElement("span");
-    spanblackEditor.classList.add("projectRemove");
-    spanblackEditor.textContent = "Mode édition";
+    spanblackEditor.classList.add("projectEdition");
+    spanblackEditor.textContent = "Modifier";
 
     //Créer Le SPAN avec le "i"
     const iconblackEditor = document.createElement("i");
@@ -216,12 +227,12 @@ const adminHTML = () => {
 
     //SPAN "Mode édition" en dessou de Sophie
     const spanFigure = spanblackEditor.cloneNode(true);
-    spanFigure.classList.remove("projectRemove");
-    spanFigure.classList.add("figureRemove");
+    spanFigure.classList.remove("projectEdition");
+    spanFigure.classList.add("figureEdition");
     //SPAN "Mode édition" des Projets
     const spanTitleProject = spanblackEditor.cloneNode(true);
-    spanTitleProject.classList.remove("projectRemove");
-    spanTitleProject.setAttribute("id", "titleProjectRemove");
+    spanTitleProject.classList.remove("projectEdition");
+    spanTitleProject.setAttribute("id", "titleProjectEdition");
 
     //INJECTION  SPAN
     figure.appendChild(spanFigure);
@@ -259,3 +270,309 @@ const adminHTML = () => {
 };
 
 
+
+
+
+function openModal() {
+
+    document.getElementById("modalworks").innerHTML = "";
+
+    //INJECTION DES ELEMENTS FETCHER
+
+    const imagesUrl = [...document.querySelectorAll(".gallery img")].map((img) =>
+        img.getAttribute("src")
+    );
+    const imagesUrlSet = new Set(imagesUrl);
+
+    //INJECTIONS DES CARTES DS MODAL
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    const imageElements = [...imagesUrlSet].map((link, index) => {
+        const container = document.createElement("figure");
+        const img = document.createElement("img");
+        const p = document.createElement("p");
+        const iconDelete = document.createElement("i");
+
+        // ajouter l'attribut data-card-id
+
+        container.setAttribute("data-card-id", works[index].id);
+        iconDelete.id = "deleteIcon";
+        iconDelete.classList.add("fa-solid", "fa-trash-can", "iconModal");
+        iconDelete.setAttribute("aria-hidden", "true");
+        img.src = link;
+        p.textContent = "éditer";
+        container.appendChild(img);
+        container.appendChild(p);
+        container.appendChild(iconDelete);
+
+        // Ajouter l'icône de déplacement uniquement sur le premier élément
+        if (index === 0) {
+            const iconMove = document.createElement("i");
+            iconMove.id = "moveIcon";
+            iconMove.classList.add(
+                "fa-solid",
+                "fa-arrows-up-down-left-right",
+                "iconModal"
+            );
+            container.appendChild(iconMove);
+        }
+
+        return container;
+    });
+
+    const galleryMap = document.getElementById("modalworks");
+    galleryMap.append(...imageElements);
+}
+
+
+// Ouverture de la page d'ajout des photos à partir de la modale
+function AddpicModal() {
+    const addProject = document.getElementById("AddpicModal");
+    const inputFile = document.getElementById("filetoUpload");
+    const selectCategory = document.getElementById("category");
+    const editSection = document.querySelector("#editSection");
+    const addToApi = document.getElementById("addWorks");
+    const gallerySection = document.querySelector("#modalContent");
+    const previewModal = document.querySelector("#previewModal");
+    let iCanSubmit = false;
+
+    //Cache - Cache differentes section Madale
+    addProject.addEventListener("click", () => {
+        gallerySection.style.display = "none";
+        editSection.style.display = "";
+        previewModal.style.display = "initial";
+    });
+
+    previewModal.addEventListener("click", () => {
+        gallerySection.style.display = "";
+        editSection.style.display = "none";
+        previewModal.style.display = "none";
+    });
+
+    //PARTIE IMG
+    inputFile.addEventListener("change", addPicture);
+
+    //PARTIE CATEGORIE
+
+    // Utiliser les données de l'API du 2e Fetch pour générer les options de l'élément select
+
+    if (selectCategory.options.length === 0) {
+        const emptyOption = document.createElement("option");
+        emptyOption.value = "";
+        emptyOption.textContent = "";
+        selectCategory.appendChild(emptyOption);
+
+        categories.forEach((category) => {
+            const option = document.createElement("option");
+            option.textContent = category.name;
+            option.setAttribute("data-id", category.id);
+            selectCategory.appendChild(option);
+        });
+    }
+    // Condition Formulaire POST
+
+
+    editSection.addEventListener("input", () => {
+        const editTitle = document.querySelector("#title");
+        const errorImg = document.getElementById("errorImg");
+        const titleError = document.querySelector("#ErrorTitleSubmit");
+        const categoryError = document.querySelector("#ErrorCategorySubmit");
+        const submitForm = document.querySelector(
+            "#Works > div.footerModal.editFooter > input[type=submit]"
+        );
+        iCanSubmit = false;
+        titleSelected = false;
+        categorySelected = false;
+        submitForm.style.background = " grey";
+        let category = document.querySelector("#category").value;
+        const title = editTitle.value;
+        const image = inputFile.files[0];
+
+
+        if (image === null || image === undefined) {
+            errorImg.textContent = "Veuillez selectionnez une image";
+            imageSelected = false;
+        } else if (title.length < 1) {
+            titleError.textContent = "Ajoutez un titre";
+            titleSelected = false;
+        } else if (category === "") {
+            categoryError.textContent = "Choisissez une catégorie";
+            titleError.textContent = "";
+            categorySelected = false;
+        } else {
+
+            titleError.textContent = "";
+            categoryError.textContent = "";
+            categorySelected = true;
+            titleSelected = true;
+            imageSelected = true;
+
+
+        }
+        if (titleSelected && categorySelected && imageSelected) {
+            submitForm.style.background = " #1d6154";
+            iCanSubmit = true;
+        }
+    });
+
+    addToApi.addEventListener("submit", (e) => {
+        e.preventDefault();
+        //Récupérer les valeurs INPUTs
+        if (iCanSubmit) {
+            //Récupérer image
+            const image = inputFile.files[0];
+
+            //Récupérer Titre
+            const title = document.querySelector("#title").value;
+
+            //Récupérer id du fetch Category depuis la liste
+            let categorySelect = document.querySelector("#category");
+            let selectedOption = categorySelect.selectedOptions[0];
+            let category = selectedOption.getAttribute("data-id");
+            category = parseInt(category);
+
+            const formData = new FormData();
+            formData.append("image", image);
+            formData.append("title", title);
+            formData.append("category", category);
+            //console.log(formData);
+
+            fetch(backEndApiURL + "works", {
+                method: "POST",
+                headers: {
+                    Accept: "application/json",
+                    Authorization: "Bearer " + token,
+                },
+                body: formData,
+            })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error("Requête NON passée");
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log("Requête passée", data);
+                    fetchWorks();
+                    displayWorks();
+                    closeModal();
+                    // réinitialiser le champ inputFile sinon il envoie plusieur formData en post
+                    inputFile.value = "";
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                    console.log("Ta requête POST non passée");
+                });
+        } else {
+            console.log("Formulaire invalide");
+        }
+    });
+}
+
+function disableScroll() {
+    document.body.classList.add("modalOpen");
+}
+
+function enableScroll() {
+    document.body.classList.remove("modalOpen");
+}
+
+function displayModal() {
+    const modal = document.querySelector("#modal");
+    const closeModalBtn = document.querySelector("#closeModal");
+    closeModalBtn.addEventListener("click", closeModal);
+    window.addEventListener("click", (e) => {
+        if (e.target === modal) closeModal();
+    });
+    disableScroll();
+}
+function closeModal() {
+    document.getElementById("modal").remove();
+    enableScroll();
+}
+
+const modalHTML = () => {
+    document.body.insertAdjacentHTML(
+        "beforeend",
+        `
+            <aside id="modal" class="modal" role="dialog" aria-labelledby="modalTitle" aria-hidden="true" display="initial">
+    <div id="modalBlock">
+      <i id="closeModal" class="fa-solid fa-xmark"></i>
+      <i id="previewModal" class="fa-solid fa-arrow-left "></i>
+      
+      <!-- GALERIE PHOTO -->
+      <section class="modalTemplate" id="modalContent">
+        <div id="editionGallery">
+          <h2 class="modalTitle">Galerie photo</h2>
+         
+          <div id="modalworks">
+          </div>
+        </div>
+        <div class="footerModal">
+          <hr>
+          <input type="submit" value="Ajouter une photo" id="AddpicModal">
+          <p id="deleteAllWorks">Supprimer la gallerie</p>
+        </div>
+      </section>
+      <!-- EDIT PHOTO -->
+      <section class="modalTemplate" id="editSection" style="display:none">
+        <h2 class="modalTitle">Ajout photo</h2>
+        <form id="editWorks">
+          <div id="addImageContainer">
+            <i class="fa-solid fa-image"></i>
+            <div id="inputFile">
+              <label for="filetoUpload" class="fileLabel">
+                <span>+ Ajouter une photo</span>
+                <input type="file" id="filetoUpload" name="image" accept="image/png, image/jpeg"
+                  class="file-input">
+              </label>
+            </div>
+            <span class="filesize">jpg, png : 4mo max</span>
+            <span id="errorImg"></span>
+          </div>
+          <div class="inputEdit" id="addTitle">
+            <label for="title">Titre</label>
+            <input type="text" name="title" id="title" class="inputCss" required>
+            <span id="ErrorTitleSubmit" class="errormsg"></span>
+          </div>
+          <div class=" inputEdit" id="addCategory">
+            <label for="category">Catégorie</label>
+            <select name="category" id="category" data-id="" class="inputCss"></select>
+            <span id="ErrorCategorySubmit" class="errormsg"></span>
+          </div>
+          <div class="footerModal editFooter">
+            <hr>
+            <input type="submit" value="Valider">
+          </div>
+        </form>
+      </section>
+    </div>
+  </aside>
+            `
+    );
+};
+const addPicture = () => {
+    const inputFile = document.getElementById("filetoUpload");
+    const viewImage = document.getElementById("addImageContainer");
+    const file = inputFile.files[0];
+    // 4Mo en octets => Message ERROR
+    const maxSize = 4 * 1024 * 1024;
+
+    if (file.size > maxSize) {
+        errorImg.textContent = "Votre image est trop volumineuse";
+        return;
+    }
+
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        viewImage.innerHTML = "";
+        const img = document.createElement("img");
+        img.setAttribute("src", reader.result);
+        viewImage.appendChild(img);
+        viewImage.style.padding = "0";
+    });
+
+    reader.readAsDataURL(file);
+};
